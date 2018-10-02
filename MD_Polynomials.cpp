de7e9c927cb46d6dc542c74776cd0050ff1f50dc
Matthew Drajeske <mattdrajeske@gmail.com>
2018-10-02 10:37:05 -0600

    First commit

---
+++ MD_Polynomials.cpp
@@ -0,0 +1,90 @@
+
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+//#include <string>
+
+using namespace std;
+
+void find_root(double&, double&, double&);
+bool isImaginary(double&);
+
+int main()
+{
+	bool valid = true;
+
+	//double root1, root2;
+	double A, B, C;
+
+	cout << "Welcome to my polynomial root program!" << endl;
+
+	do {
+
+		if (!valid) {
+			cout << "\nPlease only enter a numerical value for coefficients" << endl;
+		}
+
+		valid = true;
+
+		cout << "\nPlease enter the coefficients for a polynomial" << endl;
+
+		cout << "\na:  ";
+		cin >> A;
+
+		cout << "\nb:  ";
+		cin >> B;
+
+		cout << "\nc:  ";
+		cin >> C;
+
+		if (cin.fail()) {
+			valid = false;
+			cin.clear();
+			cin.ignore();
+
+			system("CLS");
+		}
+		
+	} while (!valid);
+
+	find_root(A, B, C);
+
+	cout << setprecision(4);
+	cout << fixed;
+
+	if (C < 0) {
+		cout << "\nYour roots are " << setw(4) << A << " + " << setw(4) << B << "i" <<
+			" and " << setw(4) << A << "-" << setw(4) << B << "i";
+	}
+	else {
+		cout << "\nYour roots are " << setw(4) << A <<
+			" and "<< setw(4) << B;
+	}
+
+	return 0;
+}
+
+void find_root(double& A, double& B, double& C) {
+
+	double root1, root2;
+	double discriminant = pow(B, 2) - (4 * A * C);
+	double b2a = -B / (2 * A);
+	
+	C = discriminant; //to evaluate whether roots are imaginary
+
+	if (discriminant < 0) {
+		
+		A = b2a;
+		B = sqrt(abs(discriminant));
+		
+	}
+	else {
+		
+		A = b2a + sqrt(discriminant);
+		B = b2a - sqrt(discriminant);
+	}
+
+}
+
